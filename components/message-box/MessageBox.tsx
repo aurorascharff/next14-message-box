3a73239aac291b535ca9bebf644d385a14a6f489
Aurora Scharff Walberg <aurora.walberg@inmeta.no>
2024-04-28 00:36:57 +0200

    Fix css

--- components/message-box/MessageBox.tsx
+++ components/message-box/MessageBox.tsx
@@ -13,7 +13,7 @@ export default async function MessageBox() {
     <div className="flex w-full flex-col shadow-xl">
       <h1 className="bg-slate-500 p-6 text-lg text-white">Messages</h1>
       <div className="grid border-x border-b border-gray-300">
-        <DomNodeListener className="grid h-64 gap-4 overflow-auto p-4">
+        <DomNodeListener className="grid h-80 content-start gap-4 overflow-auto p-4">
           {messages.length === 0 && <div className="grid place-content-center text-gray-500">No messages</div>}
           {messages.map(message => {
             return <MessageDisplay userId={user.id} key={message.id} message={message} />;
