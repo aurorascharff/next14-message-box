1ff9ef2e317b2ffd50d75425dc1867f18f906b37
Aurora Scharff <aurora.sofie@gmail.com>
2024-07-07 21:07:23 +0100

    Update

--- components/message-box/MessageBox.tsx
+++ components/message-box/MessageBox.tsx
@@ -10,7 +10,9 @@ export default async function MessageBox() {
 
   return (
     <div className="flex w-full flex-col shadow-xl sm:w-[400px]">
-      <h1 className="bg-slate-500 p-6 text-lg text-white">Messages</h1>
+      <div className="flex justify-between bg-slate-500 p-6">
+        <h1 className="text-lg text-white">Messages</h1>
+      </div>
       <div className="grid border-x border-b border-gray-300">
         <div className="grid h-80 content-start gap-4 overflow-auto border-b border-gray-300 p-4">
           {messages.length === 0 && <span className="text-center text-gray-500">No messages</span>}
--- components/message-box/MessageInput.tsx
+++ components/message-box/MessageInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../Button';
+import Button from '../ui/Button';
 
 type Props = {
   userId: string;
